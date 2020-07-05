7cf320544016af573bd12979c3759c28a2f6efcf
William A <wsashbee@gmail.com>
2020-07-04 23:45:30 -0400

    end of day commit, changed bounding box, isn'y square, but seems same regardless of circle, so i will probably use it.

--- continuous-circle-draw.py
+++ continuous-circle-draw.py
@@ -1,8 +1,13 @@
 from matplotlib.patches import Circle
 import matplotlib.pyplot as plt
-e = Circle( xy=(0,0), radius=1 )
+from matplotlib.transforms import Bbox
+import numpy as np
+e = Circle( xy=(.2,.2), radius=.1 )
 ax = plt.gca()  # ax = subplot( 1,1,1 )
 ax.add_artist(e)
+#print(ax.bbox)
+bb = ax.bbox
+bb._bbox = Bbox(np.array([[0.0, 0.0], [1.0, 1.0]], float))
 e.set_clip_box(ax.bbox)
 e.set_edgecolor( "black" )
 e.set_facecolor( "none" )  # "none" not None
