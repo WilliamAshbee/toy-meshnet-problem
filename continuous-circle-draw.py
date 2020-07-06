1f2d6cf128100299d0966603ead48b4ee8a16619
William A <wsashbee@gmail.com>
2020-07-05 22:22:05 -0400

    generates circles w/ csv labels

--- continuous-circle-draw.py
+++ continuous-circle-draw.py
@@ -2,15 +2,35 @@ from matplotlib.patches import Circle
 import matplotlib.pyplot as plt
 from matplotlib.transforms import Bbox
 import numpy as np
-e = Circle( xy=(.2,.2), radius=.1 )
-ax = plt.gca()  # ax = subplot( 1,1,1 )
-ax.add_artist(e)
-#print(ax.bbox)
-bb = ax.bbox
-bb._bbox = Bbox(np.array([[0.0, 0.0], [1.0, 1.0]], float))
-e.set_clip_box(ax.bbox)
-e.set_edgecolor( "black" )
-e.set_facecolor( "none" )  # "none" not None
-e.set_alpha( 1 )
-plt.axis('off')
-plt.savefig("test.png", bbox_inches='tight')
+import random
+import csv
+with open('labels.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Filename", "i", "x","y","r"])
+                
+    for i in range(50000):
+        if i % 1000 == 0:
+            print(i)
+        fig = plt.figure()
+
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+        r = random.uniform(0, 1)
+        name = "circleixyr_{}_{:.3f}_{:.3f}_{:.3f}".format(i,x,y,r)
+        e = Circle( xy=(x, y), radius= r )
+        ax = plt.gca()  # ax = subplot( 1,1,1 )
+        ax.add_artist(e)
+        #print(ax.bbox)
+        bb = ax.bbox
+        bb._bbox = Bbox(np.array([[0.0, 0.0], [1.0, 1.0]], float))
+        e.set_clip_box(ax.bbox)
+        e.set_edgecolor( "black" )
+        e.set_facecolor( "none" )  # "none" not None
+        e.set_alpha( 1 )
+        plt.axis('off')
+        base = '/data/mialab/users/washbee/circles/'
+        plt.savefig("{}{}.png".format(base,name), bbox_inches='tight')
+        plt.close(fig)
+
+        writer.writerow(["{}{}.png".format(base,name), i, x, y, r])
+        
